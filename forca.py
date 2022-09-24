21d643b5422be963b6026ca587d43f4a88e9b3c3
Bruno <brunorodriguesdias@gmail.com>
2022-09-24 19:15:53 -0300

    iniciando o jogo e criando arquivo palavras.txt

---
+++ forca.py
@@ -0,0 +1,13 @@
+import random
+
+enforcado = False
+acertou = False
+tentativas = 0
+
+
+print("***** Bem vindo ao jogo da forca! *****")
+print("\n*** Tututorial: ***")
+print("O Jogo irá gerar uma palavra secreta, você irá digitar as letras para tentar adivinhar a palavra")
+print("Cada acerto, aparecerá como dica, você pode errar 5 vezes antes de ser enforcado!")
+print("*** Boa sorte! ***")
+
