2b2859085135432d5483877a95afdea403c26cbe
Bruno <brunorodriguesdias@gmail.com>
2022-09-25 19:43:45 -0300

    criando a lógica de escolher uma palavra aleatória no palavras.txt

--- src/forca.py
+++ src/forca.py
@@ -2,12 +2,25 @@ import random
 
 enforcado = False
 acertou = False
-tentativas = 0
+numero_tentativas = 0
 
+
+
+with open("palavras.txt", "r") as arquivo:
+    palavras = []
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+numero = random.randrange(0, len(palavras))
+palavra_secreta = palavras[numero].upper()
+
+acertos = ["_" for letra in palavra_secreta]
 
 print("***** Bem vindo ao jogo da forca! *****")
 print("\n*** Tututorial: ***")
 print("O Jogo irá gerar uma palavra secreta, você irá digitar as letras para tentar adivinhar a palavra")
 print("Cada acerto, aparecerá como dica, você pode errar 5 vezes antes de ser enforcado!")
-print("*** Boa sorte! ***")
+print("*** Boa sorte! ***\n")
 
+print(acertos)
