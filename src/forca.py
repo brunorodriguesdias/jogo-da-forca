d594c10b85690a392af8767b09d02e2a83549a85
Bruno <brunorodriguesdias@gmail.com>
2022-09-27 14:26:47 -0300

    criando funções para organizar o código e melhorando apresentação

--- src/forca.py
+++ src/forca.py
@@ -1,17 +1,11 @@
 import random
+from funcoes import *
 
 enforcado = False
 acertou = False
 numero_tentativas = 0
 
-with open("palavras.txt", "r") as arquivo:
-    palavras = []
-    for linha in arquivo:
-        linha = linha.strip()
-        palavras.append(linha)
-
-numero = random.randrange(0, len(palavras))
-palavra_secreta = palavras[numero].upper()
+palavra_secreta = gera_palavra_secreta()
 
 acertos = ["_" for letra in palavra_secreta]
 
@@ -29,15 +23,11 @@ while(not enforcado and not acertou):
     tentativa = tentativa.strip().upper()
 
     if (tentativa in palavra_secreta):
-        i = 0
-        for letra in palavra_secreta:
-            if (tentativa.upper() == letra.upper()):
-                acertos[i] = letra
-            i += 1
-        print(acertos)
+        consulta_acerto(palavra_secreta, tentativa, acertos)
     else:
         numero_tentativas += 1
         print("Você errou, ainda restam {} tentativas".format(6 - numero_tentativas))
+        impressao_forca(numero_tentativas)
     enforcado = numero_tentativas == 6
     acertou = "_" not in acertos
 
@@ -45,3 +35,5 @@ if(acertou):
     print("Parabéns, você acertou, a palavra secreta era {}".format(palavra_secreta))
 else:
     print("Você não acertou a palavra secreta!")
+
+
---
+++ src/funcoes.py
@@ -0,0 +1,62 @@
+import random
+
+def gera_palavra_secreta():
+    with open("palavras.txt", "r") as arquivo:
+        palavras = []
+        for linha in arquivo:
+            linha = linha.strip()
+            palavras.append(linha)
+
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].upper()
+    return palavra_secreta
+def consulta_acerto(palavra_secreta, tentativa, acertos):
+    i = 0
+    for letra in palavra_secreta:
+        if (tentativa.upper() == letra.upper()):
+            acertos[i] = letra
+        i += 1
+    print(acertos)
+
+
+def impressao_forca(numero_tentativas):
+    print("  _______     ")
+    print(" |/      |    ")
+
+    if(numero_tentativas == 1):
+        print(" |      (_)   ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
+
+    if(numero_tentativas == 2):
+        print(" |      (_)   ")
+        print(" |      /     ")
+        print(" |            ")
+        print(" |            ")
+
+    if(numero_tentativas == 3):
+        print(" |      (_)   ")
+        print(" |      /|    ")
+        print(" |            ")
+        print(" |            ")
+
+    if(numero_tentativas == 4):
+        print(" |      (_)   ")
+        print(" |      /|\   ")
+        print(" |            ")
+        print(" |            ")
+
+    if(numero_tentativas == 5):
+        print(" |      (_)   ")
+        print(" |      /|\   ")
+        print(" |      /     ")
+
+    if(numero_tentativas == 6):
+        print(" |      (_)   ")
+        print(" |      /|\   ")
+        print(" |      / \    ")
+
+    print(" |            ")
+    print("_|___         ")
+    print()
